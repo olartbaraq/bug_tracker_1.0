b2562d281c53c8d4125e3833cac63b6a611814ab
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-06-09 07:52:34 +0100

    ub

--- all_file_run.py
+++ all_file_run.py
@@ -3,7 +3,7 @@ from main_files import create_app, db
 app = create_app()
 with app.app_context():
     # db.drop_all()
-    # db.create_all()
+    db.create_all()
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=False)
